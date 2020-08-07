2817da98f1f4d9e26d3c6ff65ad0ee7dd5bf485b
William Phokompe <phokompe.william@outlook.com>
2020-08-07 12:58:51 +0200

    Item context created

--- .NET/SimpleApi/Models/ItemContext.cs
+++ .NET/SimpleApi/Models/ItemContext.cs
@@ -1,5 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 
 namespace SimpleApi.Models {
-    
+    public class ItemContext : DbContext {
+        public ItemContext(DbContextOptions<ItemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ItemContext> Items { get; set; }
+    }
 }
