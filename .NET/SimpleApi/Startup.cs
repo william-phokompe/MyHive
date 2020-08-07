a6003e3fe09c98894f429427bb35c521131b475b
William Phokompe <phokompe.william@outlook.com>
2020-08-07 15:01:55 +0200

    Integrate Swashbuckle to add and configure Swagger to the middleware

--- .NET/SimpleApi/Startup.cs
+++ .NET/SimpleApi/Startup.cs
@@ -30,11 +30,24 @@ namespace SimpleApi
         {
             services.AddDbContext<ItemContext>(opt => opt.UseInMemoryDatabase("ItemList"));
             services.AddControllers();
+
+            // Register the Swagger, defining on one or more docs
+            services.AddSwaggerGen(sGen => {
+                sGen.SwaggerDoc("v1", new OpenApiInfo { Title = "My Simple API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to server generated Swagger as a JSON endpoint
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS ... ), specifying the Swagger JSON endpoint
+            app.UseSwaggerUI(sUI => {
+                sUI.SwaggerEndpoint("/swagger/v1/swagger.json", "My Simple API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
