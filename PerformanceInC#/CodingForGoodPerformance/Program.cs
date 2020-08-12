5e80d70110ebe9721854d73142a5cbb83c0fabba
William Phokompe <phokompe.william@outlook.com>
2020-08-12 02:02:49 +0200

    Measure hashing methods marked with Benchamrk attribute

--- PerformanceInC#/CodingForGoodPerformance/Program.cs
+++ PerformanceInC#/CodingForGoodPerformance/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
 
 namespace CodingForGoodPerformance
 {
+    public class Md5VsSha256 {
+        private const int N = 10000;
+        private readonly byte[] data;
+
+        private readonly SHA256 sha256 = SHA256.Create();
+        private readonly MD5 mD5 = MD5.Create();
+
+        public Md5VsSha256() {
+            data = new byte[N];
+            new Random(42).NextBytes(data);
+        }
+
+        [Benchmark]
+        public byte[] Sha256() => sha256.ComputeHash(data);
+        
+        [Benchmark]
+        public byte[] Md5() => mD5.ComputeHash(data);
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var summary = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new DebugInProcessConfig());
+            // var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+            // Console.WriteLine(summary);
         }
     }
 }
