93af744b4060708c57b3c0d86b8205d357e9bb1e
William Phokompe <phokompe.william@outlook.com>
2020-08-19 01:00:17 +0200

    Create a connection string property to create a common connection

--- WizardEmporim/WizardEmporium.User/WizardEmporium.Common/SharedRepository/BaseRepo.cs
+++ WizardEmporim/WizardEmporium.User/WizardEmporium.Common/SharedRepository/BaseRepo.cs
@@ -1,3 +1,11 @@
 public abstract class BaseRepo {
-    
+
+    private readonly string connectionString;
+
+    public BaseRepo(string connectionString) {
+        if (string.IsNullOrWhiteSpace(connectionString))
+            throw new NotSupportedException("Please specify connection string.");
+        this.connectionString = connectionString;
+    }
+
 }
